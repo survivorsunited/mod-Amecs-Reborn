ee7d0eb04836718d41bfe41fdf700a3a741a4cf2
mkram17 <1maxkramer@gmail.com>
2025-11-18 18:23:21 -0500

    mc 1.21.10 bump

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,7 +20,7 @@ stonecutter {
     centralScript = "build.gradle.kts"
 
     create(rootProject) {
-        versions("1.21.1", "1.21.3", "1.21.4", "1.21.5")
+        versions("1.21.1", "1.21.3", "1.21.4", "1.21.5", "1.21.10")
         vcsVersion = "1.21.5"
     }
 }

046182bef65ea1acbb2d326d4c23374a3e26e95c
Max Barrass <max.barrass@gmail.com>
2026-06-29 18:19:39 +1000

    Register active Stonecutter node for branch build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,7 +20,7 @@ stonecutter {
     centralScript = "build.gradle.kts"
 
     create(rootProject) {
-        versions("1.21.11")
-        vcsVersion = "1.21.11"
+        versions("1.21.5", "1.21.11")
+        vcsVersion = "1.21.5"
     }
 }

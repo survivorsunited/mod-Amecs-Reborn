efe50166dc512b4d942854664eec222ddceebaed
Max Barrass <max.barrass@gmail.com>
2026-07-03 00:34:49 +1000

    Port keybinds screen without disabling features

--- src/main/java/de/siphalor/amecs/mixin/MixinKeybindsScreen.java
+++ src/main/java/de/siphalor/amecs/mixin/MixinKeybindsScreen.java
@@ -18,30 +18,30 @@ package de.siphalor.amecs.mixin;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import de.siphalor.amecs.api.AmecsKeyBinding;
-import net.minecraft.client.gui.screen.option.ControlsListWidget;
-import net.minecraft.client.gui.screen.option.KeybindsScreen;
-import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.input.KeyInput;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
 import de.siphalor.amecs.api.KeyModifier;
 import de.siphalor.amecs.api.KeyModifiers;
 import de.siphalor.amecs.impl.duck.IKeyBinding;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.Click;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.option.ControlsListWidget;
 import net.minecraft.client.gui.screen.option.GameOptionsScreen;
+import net.minecraft.client.gui.screen.option.KeybindsScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.input.KeyInput;
 import net.minecraft.client.option.GameOptions;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.Text;
 import net.minecraft.util.Util;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
  * @author Siphalor
@@ -58,8 +58,28 @@ public abstract class MixinKeybindsScreen extends GameOptionsScreen {
 
 	@Shadow private ControlsListWidget controlsList;
 
+	@Shadow private ButtonWidget resetAllButton;
+
 	public MixinKeybindsScreen(Screen screen, GameOptions gameOptions, Text text) {
 		super(screen, gameOptions, text);
+	}
+
+	@Inject(method = "render", at = @At("HEAD"))
+	private void amecs$ensureResetAllButton(DrawContext context, int mouseX, int mouseY, float deltaTicks, CallbackInfo callbackInfo) {
+		if (this.resetAllButton == null) {
+			this.resetAllButton = ButtonWidget.builder(Text.translatable("controls.resetAll"), button -> {
+				for (KeyBinding keyBinding : this.gameOptions.allKeys) {
+					keyBinding.setBoundKey(keyBinding.getDefaultKey());
+					((IKeyBinding) keyBinding).amecs$getKeyModifiers().unset();
+					if (keyBinding instanceof AmecsKeyBinding amecsKeyBinding) {
+						amecsKeyBinding.resetKeyBinding();
+					}
+				}
+				if (this.controlsList != null) {
+					this.controlsList.update();
+				}
+			}).dimensions(0, 0, 150, 20).build();
+		}
 	}
 
 	@Inject(
